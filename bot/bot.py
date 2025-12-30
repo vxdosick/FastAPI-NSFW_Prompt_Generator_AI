12b3d92a1c315255bb1e1e9d94c87698a66d2848
vxdosick <vadumfotenuk@gmail.com>
2025-12-30 09:41:48 +0100

    Prepare project to render test deploy

---
+++ bot/bot.py
@@ -0,0 +1,154 @@
+# Imports
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from telegram.constants import ChatType
+from dotenv import load_dotenv
+from openai import OpenAI
+
+import sys
+import os
+import time
+import httpx
+
+# Dotenv, storage path connecting
+load_dotenv()
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from storage.storage import load_users, save_users, START_CREDITS
+
+# Define tokens
+TOKEN = os.getenv("TB_TOKEN")
+
+# Functions
+user_last_action = {}
+def is_rate_limited(user_id, cooldown=3):
+    now = time.time()
+    last = user_last_action.get(user_id, 0)
+    if now - last < cooldown:
+        return True
+    
+    user_last_action[user_id] = now
+    return False
+
+# Requests (functions in telegram)
+async def start(update, context):
+    if update.effective_chat.type != ChatType.PRIVATE:
+        return
+    
+    user_id = str(update.effective_user.id)
+    users = load_users()
+
+    if user_id not in users:
+        users[user_id] = {
+            "credits": START_CREDITS
+        }
+        save_users(users)
+
+    await update.message.reply_text(f"""!ТУТ БУДЕТ ПРИВЕТСТВИЕ, ПРЕДНАЗНАЧЕНИЕ БОТА И ФУНКЦИИ
+                                    (КРЕДИТОВ ОСТАЛОСЬ: {users[user_id]["credits"]})""")
+
+async def help(update, context):
+    if update.effective_chat.type != ChatType.PRIVATE:
+        return
+    await update.message.reply_text("!ТУТ БУДЕТ ИНФОРМАЦИЯ О БОТЕ, ПОМОЩЬ ГРУБО ГОВОРЯ")
+
+async def credits(update, context):
+    if update.effective_chat.type != ChatType.PRIVATE:
+        return
+    user_id = str(update.effective_user.id)
+    users = load_users()
+
+    credits = users.get(user_id, {}).get("credits", 0)
+
+    await update.message.reply_text(f"У ПОЛЬЗОВАТЕЛЯ ОСТАЛОСЬ {credits} КРЕДИТОВ")
+
+async def buy(update, context):
+    user_id = str(update.effective_user.id)
+
+    async with httpx.AsyncClient() as client:
+        r = await client.post(f"http://localhost:8000/create-checkout-session/{user_id}")
+        data = r.json()
+        await update.message.reply_text(f"КУПИ 10 КРЕДИТОВ ПУПСИК - {data['url']}")
+
+async def unknown(update, context):
+    if update.effective_chat.type != ChatType.PRIVATE:
+        return
+    await update.message.reply_text("!ТУТ БУДЕТ ТЕКСТ НЕИЗВЕСНОЙ КОМАНДЫ, СПИСОК КОМАНД")
+
+# Requests (text in telegram)
+async def echo(update, context):
+    if update.effective_chat.type != ChatType.PRIVATE:
+        return
+    
+    user_id = str(update.effective_user.id)
+    users = load_users()
+    
+    if is_rate_limited(user_id):
+        await update.message.reply_text("!ТУТ БУДЕТ ТЕКСТ ОБОЗНАЧАЮЩИЙ СЛИШЬКОМ ЧАСТЫЕ ЗАПРОСЫ")
+        return
+    
+    # request to GPT 4o-mini
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=os.getenv("OPENAI_API_KEY"),
+    )
+    completion = client.chat.completions.create(
+        extra_body={},
+        model="openai/gpt-4o-mini",
+        messages = [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": f"""Take on the role of a prompt generator.
+                        You will receive requests.
+                        You must filter out requests that are in no way related to creating prompts.
+                        Based on the request,
+                        you must create the perfect prompt that will later go to any neural network.
+                        Ignore requests of a different nature.
+                        Please provide prompts strictly in English
+                        (but the languages of the queries may vary).
+                        Always respond in this format -
+                        status: good/error (good if the prompt was created successfully and error if something went wrong)
+                        prompt: and here is the prompt / and do not write anything at all if the status is error
+                        Here is a request:{update.message.text}"""
+                    }
+                ]
+            }
+        ]
+    )
+
+    result = completion.choices[0].message.content
+
+    # result parsing
+    data = {}
+
+    for line in result.strip().splitlines():
+        key, value = line.strip().split(":", 1)
+        key = key.strip()
+        value = value.strip()
+        data[key] = value
+
+    if data.get("status") == "good":
+        if users.get(user_id, {}).get("credits", 0) > 0:
+            users[user_id]["credits"] -= 1
+            save_users(users)
+            prompt = data.get("prompt")
+        # response
+        await update.message.reply_text(f"{prompt}")
+    elif data.get("status") == "error":
+        await update.message.reply_text("!ТУТ БУДЕТ ТЕКСТ ОШИБКИ ЕСЛИ ПОЛЬЗОВАТЕЛЬ НЕ ПОПРОСИЛ ПРОМПТ")
+
+# TB App creating
+app = Application.builder().token(TOKEN).build()
+
+# Define TB handlers
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("help", help))
+app.add_handler(CommandHandler("credits", credits))
+app.add_handler(CommandHandler("buy", buy))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+# (always in the end)
+app.add_handler(MessageHandler(filters.COMMAND, unknown))
+
+# Starting (dev mode)
+app.run_polling()
---
+++ server/main.py
@@ -0,0 +1,69 @@
+# Imports
+from fastapi import FastAPI, Request, HTTPException
+import stripe
+from dotenv import load_dotenv
+
+import os
+from storage.storage import load_users, save_users
+
+# Load dotenv variables
+load_dotenv()
+
+# FastAPI app creating
+app = FastAPI()
+
+# Define tokens
+stripe.api_key=os.getenv("STRIPE_TEST_SECRET_KEY")
+
+# FastAPI Endpoints
+@app.post("/create-checkout-session/{user_id}")
+async def create_checkout(user_id: str):
+    session = stripe.checkout.Session.create(
+        payment_method_types=["card"],
+        line_items=[{
+            "price_data": {
+                "currency": "eur",
+                "product_data": {
+                    "name": "10 Credits",
+                },
+                "unit_amount": 50,
+            },
+            "quantity": 1,
+        }],
+        mode="payment",
+        # save user information
+        metadata={
+            "telegram_user_id": user_id,
+            "credits": "5"
+        },
+        success_url="https://t.me/nsfw_prompt_generator_bot?start=payment_success",
+        cancel_url="https://t.me/nsfw_prompt_generator_bot?start=payment_cancel"
+    )
+    return {"url": session.url}
+
+@app.post("/stripe-webhook")
+async def stripe_webhook(request: Request):
+    payload = await request.body()
+    sig_header = request.headers.get("stripe-signature")
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload,
+            sig_header,
+            os.getenv("STRIPE_WEBHOOK_SECRET")
+        )
+    except stripe.error.SignatureVerificationError:
+        raise HTTPException(status_code=400, detail="Invalid signature")
+    
+    if event["type"] == "checkout.session.completed":
+        session = event["data"]["object"]
+        telegram_user_id = session["metadata"]["telegram_user_id"]
+        credits = int(session["metadata"]["credits"])
+
+        users = load_users()
+        if telegram_user_id not in users:
+            users[telegram_user_id] = {"credits": 0}
+        users[telegram_user_id]["credits"] += credits
+        save_users(users)
+
+    return {"status": "ok"}
